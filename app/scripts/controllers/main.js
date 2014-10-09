fec28f5c8a663d8aff6c3cad731a3fd0225260bb
Garrett Sussman <gasussman@gmail.com>
2014-10-09 10:48:08 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gcastCapstoneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gcastCapstoneApp
+ */
+angular.module('gcastCapstoneApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

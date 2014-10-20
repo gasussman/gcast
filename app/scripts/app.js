4ff1d5356d6dd9cf58066a370798499d19aa9af4
Garrett Sussman <gasussman@gmail.com>
2014-10-20 14:38:27 +0000

    more styling

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -28,21 +28,30 @@
 
 gcastCapstoneApp.controller('ChatCtrl', ['$scope', function($scope) {
       var sock = new SockJS('http://127.0.0.1:9999/chat');
-            $scope.messages = [];
-            $scope.usernames = [];
-            $scope.sendMessage = function() {
-                sock.send($scope.messageText);
-                $scope.messageText = "";
-            };
+      $scope.messages = [];
+      $scope.usernames = [];
+      $scope.username = "Guest-" + Math.floor(Math.random()*9999);
+      $scope.messageText = "";
 
-            sock.onmessage = function(e) {
-                $scope.messages.push(e.data);
-                $scope.$apply();
-            };
-      $scope.registerUser = function() {
-        $('#userModal').modal();
+      $scope.sendMessage = function() {
+          sock.send($scope.username + ": " + $scope.messageText);
+          $scope.messageText = "";
       };
 
+      sock.onmessage = function(e) {
+          $scope.messages.push(e.data);
+          $scope.$apply();
+      };
+
+      $scope.registerUser = function() {
+      
+        
+
+        $scope.username = $scope.newUsername;
+        $scope.newUsername = "";
+        if($scope.username === 'a') return false;
+        $('#userModal').modal('hide');
+      };
 
   }]);
 
